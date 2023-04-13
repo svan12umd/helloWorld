995a53a0f5149e8dc277e85153eee2b059ffd230
shannvan <svan12@terpmail.umd.edu>
2023-04-13 10:09:18 -0400

    altered create_db page to add login information and password

--- create_db.py
+++ create_db.py
@@ -23,7 +23,7 @@ with app.app_context():
             'password': generate_password_hash('managerpw', method='sha256'), 'role':'MANAGER'},
         {'username': 'admin', 'email': 'admin@umd.edu', 'first_name':'Crystal', 'last_name':'Ball',
             'password': generate_password_hash('adminpw', method='sha256'), 'role':'ADMIN'},
-        {'svan12': 'student', 'email': 'svan12@umd.edu', 'first_name': 'Shannon', 'last_name': 'Van',
+        {'username': 'svan12', 'email': 'svan12@umd.edu', 'first_name': 'Shannon', 'last_name': 'Van',
          'password': generate_password_hash('svan12', method='sha256'), 'role': 'STUDENT'}
     ]
 
